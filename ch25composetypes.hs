f63d81bbd41957f41fda471f4428dad28338344c
Ernesto Menéndez <pyalec@gmail.com>
2017-01-22 20:03:22 -0600

    partially add chapter 25, I will also skip chapter 26 for now

---
+++ ch25composetypes.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Chapter25ComposeTypes where
+
+newtype Identity a =
+  Identity  { runIdentity :: a }
+  deriving (Eq, Show)
+
+newtype Compose f g a =
+  Compose { getCompose :: f (g a) }
+  deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure a = Compose $ (pure . pure) a
+
+  (<*>) :: Compose f g (a -> b)
+        -> Compose f g a
+        -> Compose f g b
+  (Compose f) <*> (Compose a) = Compose $ fmap (<*>) f <*> a
+
+-- 25.6 Exercises: Compose Instances
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap = undefined
