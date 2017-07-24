984834c5fc0da3d40bf8540e30dcc268dc9d4fcc
Ernesto Menéndez <ernesto.menendez@xoom.com>
2017-07-23 21:01:39 -0600

    ch25: Bifunctor instances (and now for something completely different)

--- ch25composetypes.hs
+++ ch25composetypes.hs
@@ -2,6 +2,8 @@
 
 module Chapter25ComposeTypes where
 
+import Prelude hiding (Either(..))
+
 newtype Identity a =
   Identity  { runIdentity :: a }
   deriving (Eq, Show)
@@ -32,3 +34,60 @@ instance (Foldable f, Foldable g) => Foldable (Compose f g) where
 
 instance (Traversable f, Traversable g) => Traversable (Compose f g) where
   traverse f (Compose fga) = Compose <$> traverse (traverse f) fga
+
+-- And now for something completly different
+
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+-- 1
+data Deux a b = Deux a b deriving (Eq, Show)
+
+instance Bifunctor Deux where
+  bimap f g (Deux a b) = Deux (f a) (g b)
+
+-- 2
+newtype Const a b = Const a deriving (Eq, Show)
+
+instance Bifunctor Const where
+  bimap f _ (Const a) = Const (f a)
+
+-- 3
+data Drei a b c = Drei a b c deriving (Eq, Show)
+
+instance Bifunctor (Drei a) where
+  bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+-- 4
+data SuperDrei a b c = SuperDrei a b deriving (Eq, Show)
+
+instance Bifunctor (SuperDrei a) where
+  bimap f _ (SuperDrei a b) = SuperDrei a (f b)
+
+-- 5
+newtype SemiDrei a b c = SemiDrei a deriving (Eq, Show)
+
+instance Bifunctor (SemiDrei a) where
+  bimap _ _ (SemiDrei a) = SemiDrei a
+
+-- 6
+data Quadriceps a b c d = Quadzzz a b c d deriving (Eq, Show)
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+-- 7
+data Either a b = Left a | Right b
+
+instance Bifunctor Either where
+  bimap f _ (Left a) = Left (f a)
+  bimap _ g (Right b) = Right (g b)
